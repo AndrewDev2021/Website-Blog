6b9352fc166edddba8c576453252c72c8b120c1b
Andrii Poberezhnik <andrepoper2016@gmail.com>
2022-01-29 15:09:53 +0200

    Init commin. Done : frontend, DBcontext, HomeController .

--- Startup.cs
+++ Startup.cs
@@ -7,9 +7,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstWebProject.Domain;
 using FirstWebProject.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using FirstWebProject.Domain.Repositories.Abstract;
+using FirstWebProject.Domain.Repositories.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstWebProject
 {
@@ -17,24 +22,66 @@ namespace FirstWebProject
     {
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //enable config appsettings.json
             Configuration.Bind("Project", new Config());
-            //add support controllers and views
+
+            //connect the necessary functionality as services
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //connect DB context
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //setting up system identities
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //setting up authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //add services for controllers and views
             services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //displaying all errors in development mode
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
-            app.UseRouting();
-
+            //connect support for static files in the application
             app.UseStaticFiles();
 
+            //connect the routing system
+            app.UseRouting();
+
+            //connect authentication and authorization
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            //register the routes we need
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
