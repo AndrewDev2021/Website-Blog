dbeeccbde7a37e3473b4632b070ef12e67f3030f
Andrii Poberezhnik <andrepoper2016@gmail.com>
2022-02-02 15:52:20 +0200

    Add Service Controller and view , add view for Contacts page. Add SideBar View Components

---
+++ Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using FirstWebProject.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FirstWebProject.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
